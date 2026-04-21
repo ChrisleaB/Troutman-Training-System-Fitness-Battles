6058c070f4be7395df57adb795f176422bf7cef3
ChrisleaB <boyd.christinalea@gmail.com>
2026-04-21 15:30:25 -0500

    Update squat_war.py

--- squat_war.py
+++ squat_war.py
@@ -251,7 +251,7 @@ users = list(data.keys())
 
 # Page navigation
 if st.sidebar.button("View Champions"):
-    st.switch_page("pages/View_Champions.py")
+    st.switch_page("View_Champions.py")
 
 # Admin login
 with st.sidebar.expander("Admin", expanded=False):
